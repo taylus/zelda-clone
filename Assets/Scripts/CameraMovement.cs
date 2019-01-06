ee78fce2188791c931002b21fe27fe051ff703e2
taylus <therealbjohnson@gmail.com>
2019-01-06 00:58:22 -0600

    Add editor script for displaying camera bounding rect

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,10 +2,14 @@
 
 public class CameraMovement : MonoBehaviour
 {
+    [Tooltip("The target this camera follows.")]
     public Transform Target;
+
+    [Tooltip("How quickly the camera snaps to its target. 0 = never, 1 = instantly.")]
     public float Smoothing;
-    public Vector2 MinPosition;
-    public Vector2 MaxPosition;
+
+    [Tooltip("Area within which the camera should remain.")]
+    public Rect Bounds;
 
     public void LateUpdate()
     {
@@ -13,8 +17,8 @@ public class CameraMovement : MonoBehaviour
         {
             //keep the camera's z coordinate to prevent it from moving into the 2D plane and showing nothing
             Vector3 targetPosition = new Vector3(Target.position.x, Target.position.y, transform.position.z);
-            targetPosition.x = Mathf.Clamp(targetPosition.x, MinPosition.x, MaxPosition.x);
-            targetPosition.y = Mathf.Clamp(targetPosition.y, MinPosition.y, MaxPosition.y);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, Bounds.xMin, Bounds.xMax);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, Bounds.yMin, Bounds.yMax);
             transform.position = Vector3.Lerp(transform.position, targetPosition, Smoothing);
         }
     }
---
+++ Assets/Scripts/Editor/CameraBounds.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// This script draws a <see cref="CameraMovement.Bounds"/> rect in the scene view
+/// when the camera is selected in the Unity editor. This helps visualize where
+/// the camera can and cannot go during gameplay.
+/// </summary>
+[CustomEditor(typeof(CameraMovement))]
+public class CameraBounds : Editor
+{
+    public void OnSceneGUI()
+    {
+        var script = target as CameraMovement;
+        var camera = script.gameObject.GetComponent<Camera>();
+        if (camera == null || !camera.orthographic) return;
+
+        float cameraHeight = camera.orthographicSize * 2;
+        float cameraWidth = cameraHeight * camera.aspect;
+
+        var bounds = new Rect(
+            x: script.Bounds.xMin - (cameraWidth / 2), 
+            y: script.Bounds.yMin - (cameraHeight / 2),
+            width: script.Bounds.width + cameraWidth,
+            height: script.Bounds.height + cameraHeight);
+        Handles.DrawSolidRectangleWithOutline(bounds, Color.clear, Color.green);
+    }
+}
